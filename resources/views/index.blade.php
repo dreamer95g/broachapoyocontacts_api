01cd41cde1c6c664e30bb3c9cba94e6931edd55f
dreamer95g <dreamer95g@gmail.com>
2021-12-02 21:30:44 -0500

    cambiada la url en el index

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,25 +8,25 @@
         <link
             rel="icon"
             type="image/svg+xml"
-            href="https://apoyo-cuba-habana.herokuapp.com/assets/images/apoyo_cuba.5fbeaca6.jpg"
+            href="https://apoyocubahabana.herokuapp.com/assets/images/apoyo_cuba.5fbeaca6.jpg"
             {{--href="http://graph-tracking-api.test/assets/images/apoyo_cuba.5fbeaca6.jpg" --}}
         />
         <script
             type="module"
             crossorigin
 
-            src="https://apoyo-cuba-habana.herokuapp.com/assets/js/index.25111a17.js"
+            src="https://apoyocubahabana.herokuapp.com/assets/js/index.25111a17.js"
             {{-- src="http://graph-tracking-api.test/assets/js/index.25111a17.js" --}}
         ></script>
         <link
             rel="modulepreload"
-            href="https://apoyo-cuba-habana.herokuapp.com/assets/js/vendor.a9f2f0fa.js"
+            href="https://apoyocubahabana.herokuapp.com/assets/js/vendor.a9f2f0fa.js"
             {{-- href="http://graph-tracking-api.test/assets/js/vendor.a9f2f0fa.js" --}}
 
         />
         <link
             rel="stylesheet"
-            href="https://apoyo-cuba-habana.herokuapp.com/assets/css/index.4883d1ba.css"
+            href="https://apoyocubahabana.herokuapp.com/assets/css/index.4883d1ba.css"
             {{-- href="http://graph-tracking-api.test/assets/css/index.4883d1ba.css" --}}
         />
     </head>
