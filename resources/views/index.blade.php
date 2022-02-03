440d333a5f5228392e2f534411fd2d8bd78e6284
Gabry <dreamer95g@gmail.com>
2022-02-02 23:57:39 -0500

    https

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -8,26 +8,26 @@
         <link
             rel="icon"
             type="image/svg+xml"
-            {{-- href="https://apoyocubahabana.herokuapp.com/assets/images/apoyo_cuba.5fbeaca6.jpg" --}}
-            href="http://trackingapi.local/assets/images/apoyo_cuba.5fbeaca6.jpg"
+            href="https://apoyocubahabana.herokuapp.com/assets/images/apoyo_cuba.5fbeaca6.jpg"
+            {{-- href="http://trackingapi.local/assets/images/apoyo_cuba.5fbeaca6.jpg" --}}
         />
         <script
             type="module"
             crossorigin
 
-            {{-- src="https://apoyocubahabana.herokuapp.com/assets/js/index.3419bd8e.js" --}}
-            src="http://trackingapi.local/assets/js/index.3419bd8e.js"
+            src="https://apoyocubahabana.herokuapp.com/assets/js/index.3419bd8e.js"
+            {{-- src="http://trackingapi.local/assets/js/index.3419bd8e.js" --}}
         ></script>
         <link
             rel="modulepreload"
-            {{-- href="https://apoyocubahabana.herokuapp.com/assets/js/vendor.8af9aa5a.js" --}}
-            href="http://trackingapi.local/assets/js/vendor.8af9aa5a.js"
+            href="https://apoyocubahabana.herokuapp.com/assets/js/vendor.8af9aa5a.js"
+            {{-- href="http://trackingapi.local/assets/js/vendor.8af9aa5a.js" --}}
 
         />
         <link
             rel="stylesheet"
-            {{-- href="https://apoyocubahabana.herokuapp.com/assets/css/index.4883d1ba.css" --}}
-            href="http://trackingapi.local/assets/css/index.4883d1ba.css"
+            href="https://apoyocubahabana.herokuapp.com/assets/css/index.4883d1ba.css"
+            {{-- href="http://trackingapi.local/assets/css/index.4883d1ba.css" --}}
         />
     </head>
     <body>
